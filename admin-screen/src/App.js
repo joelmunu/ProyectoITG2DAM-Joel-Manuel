7711c2acf0d9064fe55758aade2cf75484395b3e
Joel Munuera Marrero <joel.munu@gmail.com>
2024-05-21 12:35:44 +0100

    feat: se ha consumido el servicio de inicio de sesión en la admin-screen

--- admin-screen/src/App.js
+++ admin-screen/src/App.js
@@ -2,13 +2,22 @@ import "./App.css";
 import Header from "./components/header/Header";
 import RentApp from "./components/rentApp/RentApp";
 import { BrowserRouter } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
       <BrowserRouter>
-        <Header></Header>
-        <RentApp clasname="RentApp"></RentApp>
+        <Header
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+        />
+        <RentApp
+          clasname="RentApp"
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+        />
       </BrowserRouter>
     </div>
   );
--- admin-screen/src/components/header/Header.jsx
+++ admin-screen/src/components/header/Header.jsx
@@ -2,20 +2,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import "../../styles/Header.css";
-const Header = () => {
+
+const Header = ({ isLoggedIn, setIsLoggedIn }) => {
   return (
     <div className="HeaderContainer">
       <div className="headerContent">
         <span className="title">Rent a car Tenerife 🏎️</span>
-        <Link to="/VistaGeneral">Vista General</Link>
-        <Link to="/Vehiculos">Vehiculos</Link>
-        <Link to="/Clientes">Clientes</Link>
-        <div>
-          <span className="title">Admin</span>
-          <button className="logout-btn">
-            <FontAwesomeIcon icon={faRightFromBracket} className="color-icon" />
-          </button>
-        </div>
+        {isLoggedIn ? (
+          <>
+            <Link to="/VistaGeneral">Vista General</Link>
+            <Link to="/Vehiculos">Vehiculos</Link>
+            <Link to="/Clientes">Clientes</Link>
+            <div>
+              <span className="title">Admin</span>
+              <button className="logout-btn" onClick={() => setIsLoggedIn(false)}>
+                <FontAwesomeIcon icon={faRightFromBracket} className="color-icon" />
+              </button>
+            </div>
+          </>
+        ) : (
+          <span className="title">Bienvenido</span>
+        )}
       </div>
     </div>
   );
